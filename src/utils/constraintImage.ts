3a7b7cb8f39fb10c95114986541541bcbdc490bf
Teerut Srithongdee <teerutsr@gmail.com>
2023-12-15 14:47:39 +0700

    Update dependencies and fix image upload in RichTextEditor component

--- src/utils/constraintImage.ts
+++ src/utils/constraintImage.ts
@@ -1,28 +1,50 @@
 import sharp from "sharp";
 
-async function constraintImage(
-  buffer: Buffer,
-  width = 1000,
-  height = 1000,
+async function constraintImage({
+  buffer,
+  width,
+  height,
   quality = 82,
-  drop = 2
-): Promise<Buffer> {
-  const done = await sharp(buffer)
-    .resize({
-      width,
-      height,
-      fit: sharp.fit.inside,
-    })
-    .png({
-      quality,
-    })
-    .toBuffer();
+  drop = 2,
+  fileLimit,
+}: {
+  buffer: Buffer;
+  width?: number;
+  height?: number;
+  quality?: number;
+  drop?: number;
+  fileLimit?: number;
+}): Promise<Buffer> {
+  try {
+    const { width: originalWidth, height: originalHeight } = await sharp(buffer).metadata();
+    const processedImage = await sharp(buffer)
+      .resize({
+        width: width ? width : undefined,
+        height: height ? height : undefined,
+        fit: sharp.fit.inside,
+      })
+      .png({
+        quality,
+      })
+      .toBuffer();
 
-  if (done.byteLength > 1000000 * 1) {
-    return constraintImage(buffer, quality - drop);
+    if (processedImage.byteLength > 1 * (fileLimit ? fileLimit * 1024 * 1024 : 1024 * 1024)) {
+      // If the image size exceeds 1 MB, recursively reduce dimensions
+      return constraintImage({
+        buffer,
+        width: originalWidth! - drop,
+        height: originalHeight! - drop,
+        quality,
+        drop,
+      });
+    }
+
+    return processedImage;
+  } catch (error) {
+    // Handle errors (e.g., log or throw a custom error)
+    console.error("Error processing image:", error);
+    throw error;
   }
-
-  return done;
 }
 
 export default constraintImage;
